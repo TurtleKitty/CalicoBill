0e4cb5885eb229d1e5745c0164f6cb87b980af58
TurtleKitty <joshua@anwu.org>
2012-05-18 14:37:24 -0400

    Added node.js start script

---
+++ bin/start/all.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd /home/calicobill/bin/start
+sh nodejs.sh
+sh perl.sh
+sh racket.sh
+sh ruby.sh
---
+++ bin/start/nodejs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd /home/calicobill/nodejs
+node_modules/forever/bin/forever start app.js 
